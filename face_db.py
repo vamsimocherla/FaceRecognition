909fd381efaa477f2efe62948291f20d86bb7787
vamsimocherla <vamsimocherla1@gmail.com>
2019-03-21 10:01:56 -0500

    Removed Face DB

--- face_db.py
+++ face_db.py
@@ -8,53 +8,17 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 img_dir = cwd + "/static/img/"
 model_dir = cwd + "/static/models/"
 
-# Load a third sample picture and learn how to recognize it.
-john_linss_image = face_recognition.load_image_file(img_dir + "john_linss.jpg")
-john_linss_face_encoding = face_recognition.face_encodings(john_linss_image)[0]
-
-# Load a fifth sample picture and learn how to recognize it.
-jones_image = face_recognition.load_image_file(img_dir + "jan_jones.png")
-jones_face_encoding = face_recognition.face_encodings(jones_image)[0]
-
-# Load a seventh sample picture and learn how to recognize it.
-williamshen_image = face_recognition.load_image_file(img_dir + "William_Shen.jpg")
-williamshen_face_encoding = face_recognition.face_encodings(williamshen_image)[0]
-
-# Load a eigth sample picture and learn how to recognize it.
-yusukewatanabe_image = face_recognition.load_image_file(img_dir + "Yusuke_Watanabe.jpg")
-yusukewatanabe_face_encoding = face_recognition.face_encodings(yusukewatanabe_image)[0]
-
-# Load a eigth sample picture and learn how to recognize it.
-krishna_image = face_recognition.load_image_file(img_dir + "krishna.jpg")
-krishna_face_encoding = face_recognition.face_encodings(krishna_image)[0]
+# Load a sample picture and learn how to recognize it.
+sample_image = face_recognition.load_image_file(img_dir + "sample.jpg")
+sample_face_encoding = face_recognition.face_encodings(sample_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    john_linss_face_encoding,
-    jones_face_encoding,
-    williamshen_face_encoding,
-    yusukewatanabe_face_encoding,
-    krishna_face_encoding
+    sample_face_encoding
 ]
 known_face_names = {
-    0: {"status": "See Ambassador", "jStatus": "アンバサダーを参照", "img": img_dir + "john_linss.jpg", "name": "John Linss",
-        "username": "jlinss", "age": "21", "gender": "Male", "tier": "Platinum Member", "jTier": "プラチナ会員",
-        "visits": "1,3", "residency": "Resident", "jResidency": "居住者", "prefix": "Mr."},
-    1: {"status": "See Ambassador", "jStatus": "アンバサダーを参照", "img": img_dir + "jan_jones.png",
-        "name": "Jan Jones Blackhurst", "username": "jblackhurst", "age": "21", "gender": "Female",
-        "tier": "Seven Star Member", "jTier": "セブンスター会員", "visits": "1,2", "residency": "Non-resident",
-        "jResidency": "非居住者", "prefix": "Mrs."},
-    2: {"status": "See Ambassador", "jStatus": "アンバサダーを参照", "img": img_dir + "William_Shen.jpg",
-        "name": "William Shen", "username": "wshen", "age": "28", "gender": "Male", "tier": "Platinum Member",
-        "jTier": "プラチナ会員", "visits": "0,2", "residency": "Resident", "jResidency": "居住者", "prefix": "Mr."},
-    3: {"status": "See Ambassador", "jStatus": "アンバサダーを参照", "img": img_dir + "Yusuke_Watanabe.jpg",
-        "name": "Yusuke Watanabe", "username": "ywatanabe", "age": "26", "gender": "Male",
-        "tier": "Seven Star Member", "jTier": "セブンスター会員", "visits": "1,3", "residency": "Resident",
-        "jResidency": "居住者", "prefix": "Mr."},
-    4: {"status": "See Ambassador", "jStatus": "アンバサダーを参照", "img": img_dir + "krishna.jpg",
-       "name": "Krishna", "username": "kmocherla", "age": "21", "gender": "Male",
-       "tier": "Seven Star Member", "jTier": "セブンスター会員", "visits": "1,3", "residency": "Non-Resident",
-       "jResidency": "居住者", "prefix": "Mr."}
+    0: {"img": img_dir + "sample.jpg", "name": "John Doe",
+        "username": "jdoe", "age": "21", "gender": "Male"}
 }
 unknown_face = {"name": "Unknown"}
 
